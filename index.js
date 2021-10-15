a38d9e9de7f8f32481cefd55ed148e78455c52cd
mateus.welter <welter.mateus@gmail.com>
2021-10-15 09:50:22 -0300

    a

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect(erro => {
-    if(erro) {
+    if (erro) {
         console.log(erro)
     } else {
         console.log('conectado com sucesso')
-        
+
         Tabelas.init(conexao)
-        
+
         const app = customExpress()
 
         app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
